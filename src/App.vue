95a5af26284da854ad53ce303fc928ed3669c3ad
yangjack <jack580936@gmail.com>
2023-01-24 18:23:04 +0800

    上傳計算器

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, shallowRef } from 'vue'
+
+import EpicEquipment from "@/components/EpicEquipment.vue";
+import LegendaryEquipment from "@/components/LegendaryEquipment.vue";
+
+const currentTab = ref(EpicEquipment)
+const tabs = ref({'傳奇裝備':LegendaryEquipment,'英雄裝備':EpicEquipment})
+
+</script>
+
+<template>
+  <main>
+    <div class="demo">
+      <ul class="nav nav-tabs  sticky-top  navbar-light  bg-light">
+        <li v-for="(component,name,index) in tabs" :key="index" class="nav-item">
+          <span @click="currentTab = component" :class="['nav-link' , { active: currentTab === component }]">{{ name }}</span>
+        </li>
+      </ul>
+      <keep-alive>
+      <component :is="currentTab" class="tab_page_bg"></component>
+      </keep-alive>
+    </div>
+  </main>
+</template>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// import "bootstraps/dist/css/bootstraps.min.css";
+import 'bootstrap';
+import "./assets/all.scss";
+
+import './assets/main.css';
+
+createApp(App).mount('#app');
