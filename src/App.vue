8a5c6b30b90530b6237e6f160ffa618fe31fb57e
t13846 <t13846@ms.sltung.com.tw>
2023-03-28 09:02:51 +0800

    add: 新增更新日誌並修改了navbar上幾個頁簽的位置，也調整了一下樣式

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <script setup>
-import { ref, shallowRef } from 'vue'
+import {ref, shallowRef} from 'vue'
 
 import EpicEquipment from "@/components/EpicEquipment.vue";
 import LegendaryEquipment from "@/components/LegendaryEquipment.vue";
 import InternalStrength from "@/components/InternalStrength.vue";
 import Ad from "@/components/Ad.vue";
-const currentTab = shallowRef(EpicEquipment)
-const tabs = shallowRef({'傳奇裝備':LegendaryEquipment,'英雄裝備':EpicEquipment,'內功':InternalStrength,'廣告':Ad})
+import ChangeLog from "@/components/ChangeLog.vue";
 
+const currentTab = shallowRef(EpicEquipment)
+const tabs = shallowRef({'傳奇裝備': LegendaryEquipment, '英雄裝備': EpicEquipment, '內功': InternalStrength})
+const rightTab = shallowRef({'廣告': Ad, '更新日誌': ChangeLog})
 </script>
 
 <template>
   <main>
     <div class="demo">
-      <ul class="nav nav-tabs  sticky-top  navbar-light  bg-light">
-        <li v-for="(component,name,index) in tabs" :key="index" class="nav-item">
-          <span @click="currentTab = component" :class="['nav-link' , { active: currentTab === component }]">{{ name }}</span>
-        </li>
+      <ul class="nav nav-tabs sticky-top navbar-light bg-light">
+        <div class="leftTab-container">
+          <ul class="nav">
+            <li v-for="(component,name,index) in tabs" :key="index" class="nav-item">
+              <span @click="currentTab = component" :class="['nav-link' , { active: currentTab === component }]">{{ name }}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="rightTab-container">
+          <ul class="nav">
+            <li v-for="(component,name,index) in rightTab" :key="index" class="nav-item">
+              <span @click="currentTab = component" :class="['nav-link' , { active: currentTab === component }]">{{ name }}</span>
+            </li>
+          </ul>
+        </div>
       </ul>
       <keep-alive>
-      <component :is="currentTab" class="tab_page_bg"></component>
+        <component :is="currentTab" class="tab_page_bg"></component>
       </keep-alive>
     </div>
   </main>
 </template>
+
+<style>
+
+</style>
---
+++ src/components/ChangelogContainer.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="changelog-container">
+    <div v-for="(log, index) in logs" :key="index" class="changelog-card">
+      <h2>{{ log.title }}</h2>
+      <ul class="changelog-list" v-show="log.show">
+        <li v-for="(change, index) in log.changes" :key="index" class="changelog-item">{{ change }}</li>
+      </ul>
+      <button @click="toggleShow(log)" class="changelog-toggle">{{ log.show ? 'Hide Changelog' : 'Show Changelog' }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    logs: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    toggleShow(log) {
+      log.show = !log.show;
+    }
+  }
+}
+</script>
+
+<style>
+.changelog-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column
+}
+
+.changelog-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  max-width: 600px;
+  width: 100%;
+}
+
+.changelog-list {
+  margin-top: 10px;
+  list-style-type: none;
+  padding-left: 0;
+}
+
+.changelog-item {
+  margin-bottom: 5px;
+}
+
+.changelog-toggle {
+  display: block;
+  margin-top: 10px;
+  background-color: #428bca;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.changelog-toggle:hover {
+  background-color: #3071a9;
+}
+</style>
