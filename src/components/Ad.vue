074f30846138a6686f27ac7ec78950caf0fdccb6
t13846 <t13846@ms.sltung.com.tw>
2023-04-18 15:31:31 +0800

    del: 刪除google AD 程式碼 del: 刪除 ConsitutionBg.vue

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -2,14 +2,7 @@
   <div class="ad">
     <p>沒錯就是廣告</p>
     <p>謝謝你來看廣告</p>
-  </div>
-  <div class="gads">
-    <ins class="adsbygoogle"
-         style="display: block"
-         data-ad-client="ca-pub-8035851123300328"
-         data-ad-format="auto"
-         data-full-width-responsive="true">
-    </ins>
+    <p>但現在還沒有廣告</p>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 6 */
 import { createApp } from 'vue';
 import App from './App.vue';
 
@@ -8,11 +7,3 @@ import "./assets/all.scss";
 import './assets/main.css';
 
 createApp(App).mount('#app');
-
-window['addAds'] = function () {
-    let childList = document.getElementsByClassName('gads');
-    for (let i = 0; i< childList.length; i++) {
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-
-};
