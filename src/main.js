3473e403d2a07a0033ffb6871a95c72340cd2107
t13846 <t13846@ms.sltung.com.tw>
2023-05-15 11:48:43 +0800

    fix: Constitution 修復了先點傳說階級，再點選英雄階級會出錯問題。 update: 刪除無用css main.css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import App from './App.vue';
 // import "bootstraps/dist/css/bootstraps.min.css";
 import 'bootstrap';
 import "./assets/all.scss";
-import './assets/main.css';
 
 createApp(App).mount('#app');
