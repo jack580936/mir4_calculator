4815d55b844071be0a544ad726a9485195d9d225
t13846 <t13846@ms.sltung.com.tw>
2023-05-15 17:23:12 +0800

    update: 使用了 vue router 設定為createWebHashHistory('mir4_calculator')

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import EpicEquipment from "@/components/EpicEquipment.vue";
 import LegendaryEquipment from "@/components/LegendaryEquipment.vue";
 import Constitution from "@/components/Constitution.vue";
@@ -10,54 +10,54 @@ import ChangeLog from "@/components/ChangeLog.vue";
 
 const routes = [
   {
-    path: '/mir4_calculator/',
+    path: '/:catchAll(.*)',
     name: '首頁',
-    redirect: '/mir4_calculator/constitution'
+    redirect: '/constitution'
   },
   {
-    path: '/mir4_calculator/legendary-equipment',
+    path: '/legendary-equipment',
     name: '傳說裝備',
     component: LegendaryEquipment
   },
   {
-    path: '/mir4_calculator/epic-equipment',
+    path: '/epic-equipment',
     name: '英雄裝備',
     component: EpicEquipment
   },
   {
-    path: '/mir4_calculator/constitution',
+    path: '/constitution',
     name: '體質',
     component: Constitution
   },
   {
-    path: '/mir4_calculator/mystery',
+    path: '/mystery',
     name: '奇緣',
     component: mystery
   },
   {
-    path: '/mir4_calculator/resource',
+    path: '/resource',
     name: '地圖資源',
     component: Resource
   },
   {
-    path: '/mir4_calculator/requests',
+    path: '/requests',
     name: '委託',
     component: Requests
   },
   {
-    path: '/mir4_calculator/other-calculate',
+    path: '/other-calculate',
     name: '一些小功能',
     component: OtherCalculate
   },
   {
-    path: '/mir4_calculator/change-log',
+    path: '/change-log',
     name: '更新日誌',
     component: ChangeLog
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('mir4_calculator'),
   routes
 });
 
