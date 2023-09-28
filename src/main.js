33aee4526d498fb64647eaac8d8c9722a9411542
t13846 <t13846@ms.sltung.com.tw>
2023-09-28 15:28:53 +0800

    add: use vite-plugin-sitemap to generate sitemap.xml

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue';
 import {createPinia} from "pinia";
 import App from './App.vue';
 import router from './router';
-import VueGtag from "vue-gtag";
+import VueGtm from '@gtm-support/vue-gtm';
 
 // import "bootstraps/dist/css/bootstraps.min.css";
 import 'bootstrap';
@@ -11,6 +11,11 @@ import "./assets/all.scss";
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
-app.use(VueGtag, {config: {id: import.meta.env.VITE_GA_TRACKING_ID}}, router);
+app.use(VueGtm, {
+  id: import.meta.env.VITE_GTM_CONTAINER_ID, // 你的 GTM 容器 ID
+  defer: false, // false 表示立即加載 GTM
+  compatibility: false, // 如果需要與舊版 GTM 相容，請設為 true
+  enabled: true, // true 表示啟用 GTM
+});
 app.mount('#app');
 
--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,71 @@
-
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import {fileURLToPath, URL} from 'node:url';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
+const names = [
+    'dragon-artifact',
+    'legendary-equipment',
+    'epic-equipment',
+    'constitution',
+    'mystery/grieving-the-death-of-a-friend',
+    'mystery/reckless-courage',
+    'resource/Mirage-Ship',
+    'resource/secret-peak',
+    'resource/magic-square',
+    'resource/snow-field-area-resource',
+    'resource/sabuk-area-resource',
+    'resource/sabuk-area-chest',
+    'resource/phantasia-area-resource',
+    'resource/phantasia-area-chest',
+    'resource/spiritual-area',
+    'resource/valley',
+    'resource/Mirage-Ship',
+    'requests/Mirage-Ship-Deck',
+    'requests/gorge-cliff-path',
+    'requests/nine-dragon-ice-field',
+    'requests/phantasia-desert',
+    'requests/sabuk-castle',
+    'requests/Mirage-Ship-Inner-Cabins',
+    'requests/Mirage-Ship-Deck',
+    'other-calculate/speed-boost',
+    'other-calculate/lvl',
+    'change-log',
+];
+const dynamicRoutes = names.map(name => `/mir4_calculator/${name}`);
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: '7414'
-  },
-  base: '/mir4_calculator/',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          if (extType === 'pdf') {
-            return `assets/pdf/[name][extname]`;
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-      }
-    }
-  },
+    plugins: [
+        vue(),
+        Sitemap({
+            'hostname': 'https://jack580936.github.io',
+            dynamicRoutes,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: '0.0.0.0',
+        port: '7414'
+    },
+    base: '/mir4_calculator/',
+    envDir: './env',
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.')[1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    }
+                    if (extType === 'pdf') {
+                        return `assets/pdf/[name][extname]`;
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+            }
+        }
+    },
 });
