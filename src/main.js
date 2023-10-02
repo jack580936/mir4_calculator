bf15c6c63f47b97830745f5611de3058d0cac09c
t13846 <t13846@ms.sltung.com.tw>
2023-10-02 14:32:48 +0800

    add: vuegtm add router

--- src/main.js
+++ src/main.js
@@ -13,9 +13,7 @@ app.use(router);
 app.use(createPinia());
 app.use(VueGtm, {
   id: import.meta.env.VITE_GTM_CONTAINER_ID, // 你的 GTM 容器 ID
-  defer: false, // false 表示立即加載 GTM
-  compatibility: false, // 如果需要與舊版 GTM 相容，請設為 true
-  enabled: true, // true 表示啟用 GTM
+  vueRouter: router,
 });
 app.mount('#app');
 
