9e41086a36d1ed517ba1fbbbe6d6e5954a21a3e6
t13846 <t13846@ms.sltung.com.tw>
2026-01-12 09:58:16 +0800

    Refactor: 將首頁邏輯移至 HomePage 元件並更新樣式

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,88 @@
 <script setup>
-import {useTabStore} from "@/store/tab.js";
-import {onMounted, ref} from "vue";
-import {storeToRefs} from "pinia";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+import { useTabStore } from "@/store/tab.js";
 import Sponsor from "@/components/Sponsor.vue";
+import dragonArtifact_image from "@/assets/dragonArtifact/傳說神笏.png";
+import HomePage from "@/components/HomePage.vue";
 
+const router = useRouter();
 const tabStore = useTabStore();
-const {currentTab, currentSideBarTab, screenWidth} = storeToRefs(tabStore);
-const showSmNavBarTab = ref(false);
+const { currentTab, currentSideBarTab, screenWidth, showSmNavBarTab } = storeToRefs(tabStore);
+
 const isSponsorModalVisible = ref(false);
+
+const getImageUrl = (url) => {
+  return new URL(`/src/assets/${url}`, import.meta.url).href;
+};
+
+
+const RouteTabs = [
+  { name: '龍神器', path: '/dragon-artifact' },
+  { name: '傳說裝備', path: '/legendary-equipment' },
+  { name: '英雄裝備', path: '/epic-equipment' },
+  { name: '體質', path: '/constitution' },
+  { name: '奇緣', path: '/mystery' },
+  { name: '地圖資源', path: '/resource' },
+  { name: '委託', path: '/requests' },
+  { name: '一些小功能', path: '/other-calculate' },
+  { name: '更新日誌', path: '/change-log' },
+];
+
+const leftRouteTabs = [
+  { name: '龍神器', path: '/dragon-artifact' },
+  { name: '傳說裝備', path: '/legendary-equipment' },
+  { name: '英雄裝備', path: '/epic-equipment' },
+  { name: '體質', path: '/constitution' },
+  { name: '奇緣', path: '/mystery' },
+  { name: '地圖資源', path: '/resource' },
+  { name: '委託', path: '/requests' },
+  { name: '一些小功能', path: '/other-calculate' }
+];
+
+const rightRouteTabs = [
+  { name: '更新日誌', path: '/change-log' },
+];
+
+const keepAliveComponentList = ['DragonArtifact', 'EpicEquipment', 'LegendaryEquipment'];
+
 const updateScreenWidth = () => {
   screenWidth.value = window.innerWidth;
 };
 
 
+
+
+const isActiveRoute = (routePath) => {
+  return currentTab.value === routePath;
+};
+
 onMounted(() => {
   updateScreenWidth();
   window.addEventListener('resize', updateScreenWidth);
-});
 
+  // Router beforeEach guard
+  router.beforeEach((to, from, next) => {
+    if (to.path === '/') {
+      return;
+    }
+    currentTab.value = to.path.match(/^\/[^\/]+/)[0];
+    next();
+  });
+});
 </script>
+
 
 <template>
   <main>
-    <div class="welcome-container" v-if="!currentTab">
-      <div class="page-options" >
-        <router-link
-          v-for="route in RouteTabs"
-          :key="route.name"
-          :to="route.path"
-          :class="['page-option', route.path.replace(/^\//, '')]"
-          @click="currentTab = route.path; showSmNavBarTab = false"
-        >
-        {{ route.name }}
-        </router-link>
-      </div>
-    </div>
+    <HomePage/>
     <div class="demo" v-if="currentTab">
       <div class="nav nav-tabs" v-if="screenWidth > 849 ">
         <ul class="leftTab-container">
-          <li v-for="route in leftRouteTabs" :key="route.name" class="nav-item" @click="currentTab = route.path">
-            <router-link :to="route.path" :class="{'nav-link': true, 'active': isActiveRoute(route.path)}">
-              {{ route.name }}
+          <li v-for="targetRoute in leftRouteTabs" :key="targetRoute.name" class="nav-item" @click="currentTab = targetRoute.path">
+            <router-link :to="targetRoute.path" :class="{'nav-link': true, 'active': isActiveRoute(targetRoute.path)}">
+              {{ targetRoute.name }}
             </router-link>
           </li>
         </ul>
@@ -65,7 +107,8 @@ onMounted(() => {
         </ul>
       </div>
       <div class="sm-nav-bar" v-else>
-        <div class="current-page" @click="showSmNavBarTab = !showSmNavBarTab" v-if="RouteTabs.find(route => route.path === currentTab) !== undefined">
+        <div class="current-page" @click="showSmNavBarTab = !showSmNavBarTab"
+             v-if="RouteTabs.find(route => route.path === currentTab) !== undefined">
           {{ RouteTabs.find(route => route.path === currentTab).name }}
         </div>
         <div class="sm-nav-bar-tab" v-if="showSmNavBarTab">
@@ -89,68 +132,9 @@ onMounted(() => {
         </keep-alive>
       </router-view>
     </div>
-    <Sponsor :visible="isSponsorModalVisible" @close="isSponsorModalVisible = false" />
+    <Sponsor :visible="isSponsorModalVisible" @close="isSponsorModalVisible = false"/>
   </main>
 </template>
-
-<script>
-
-import {useTabStore} from "@/store/tab.js";
-
-export default {
-  name: "App",
-  data() {
-    return {
-      RouteTabs: [
-        {name: '龍神器', path: '/dragon-artifact'},
-        {name: '傳說裝備', path: '/legendary-equipment'},
-        {name: '英雄裝備', path: '/epic-equipment'},
-        {name: '體質', path: '/constitution'},
-        {name: '奇緣', path: '/mystery'},
-        {name: '地圖資源', path: '/resource'},
-        {name: '委託', path: '/requests'},
-        {name: '一些小功能', path: '/other-calculate'},
-        {name: '更新日誌', path: '/change-log'},
-      ],
-      leftRouteTabs: [
-        {name: '龍神器', path: '/dragon-artifact'},
-        {name: '傳說裝備', path: '/legendary-equipment'},
-        {name: '英雄裝備', path: '/epic-equipment'},
-        {name: '體質', path: '/constitution'},
-        {name: '奇緣', path: '/mystery'},
-        {name: '地圖資源', path: '/resource'},
-        {name: '委託', path: '/requests'},
-        {name: '一些小功能', path: '/other-calculate'}
-      ],
-      rightRouteTabs: [
-        {name: '更新日誌', path: '/change-log'},
-      ],
-      keepAliveComponentList: ['DragonArtifact', 'EpicEquipment', 'LegendaryEquipment']
-    };
-  },
-  computed: {},
-  created() {
-    const tabStore = useTabStore();
-    this.$router.beforeEach((to, from, next) => {
-      if (to.path === '/') {
-        return;
-      }
-      tabStore.currentTab = to.path.match(/^\/[^\/]+/)[0];
-      next();
-    });
-  },
-  methods: {
-    getImageUrl(url) {
-      return new URL(`/src/assets/${url}`, import.meta.url).href;
-    },
-    isActiveRoute(routePath) {
-      const tabStore = useTabStore();
-      return tabStore.currentTab === routePath;
-    },
-  },
-};
-</script>
-
 
 <style scoped lang="scss">
 .sponsor-logo {
